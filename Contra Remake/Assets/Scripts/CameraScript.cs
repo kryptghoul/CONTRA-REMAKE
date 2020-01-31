c345bbc0b044e3ff059dc701aba2a463b40bcc99
HannieeBunniee <54680570+hanniee227@users.noreply.github.com>
2020-01-31 16:09:03 -0500

    Collision Map Update
    I added Collision to the map and the sample player with movement and jump so it can test out the map

---
+++ Contra Remake/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        //this.transform.position = new Vector3(player.transform.position.x, this.transform.position.y, this.transform.position.z);
+        transform.position = player.transform.position + offset;
+
+
+        //make the escape key as quit game
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+}
---
+++ Contra Remake/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody2D rd2d;
+    public float speed;
+
+    void Start()
+    {
+        rd2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    //======Function==========
+    void PlayerMovement()
+    {
+        float hozMovement = Input.GetAxis("Horizontal");
+        float vertMovement = Input.GetAxis("Vertical");
+        rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
+    }
+
+    private void OnCollisionStay2D(Collision2D collision) //jumping code
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
+            }
+        }
+    }
+}
