1d748e78b8f581b240e4d77e583c0fd725d5c8bc
Joshua.Bonilla_UCF <54584900+BeanTownCentral@users.noreply.github.com>
2020-02-08 22:49:14 -0500

    UPDATED TURRET, BULLET SIZE, LVL 2 ENEMY LAYOUT
    AS OF FEB.9TH

---
+++ Contra Remake/Assets/Scripts/Explosion.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    public GameObject explosion;
+
+	// Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+	
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.collider.tag == "Bullet")
+		{
+			Instantiate(explosion, transform.position, transform.rotation);
+		}
+		Destroy(gameObject);
+	}
+}
